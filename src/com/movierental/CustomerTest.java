b2a4a02507afdf0fdc070ce06fe02f82b7705d8c
sbilyi <kartanisoid@gmail.com>
2016-01-26 17:24:06 +0200

    Performed: Tests updated

--- src/com/movierental/CustomerTest.java
+++ src/com/movierental/CustomerTest.java
@@ -1,28 +1,57 @@
 package com.movierental;
 
-import org.testng.annotations.Test;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.Suite;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.testng.AssertJUnit.assertEquals;
 
 public class CustomerTest {
 
+    private Customer customer;
+
+    @Before
+    public void setup() {
+        customer = new Customer("Sergii");
+
+        Rental r1 = new Rental(new Movie("Matrix", Movie.REGULAR), 4);
+        Rental r2 = new Rental(new Movie("Matrix 2", Movie.NEW_RELEASES), 7);
+        Rental r3 = new Rental(new Movie("Panda Kung Fu", Movie.CHILDRENS), 2);
+
+        customer.addRental(r1);
+        customer.addRental(r2);
+        customer.addRental(r3);
+    }
+
+    @After
+    public void tearDown() {
+        customer = null;
+    }
+
     @Test
     public void testStatement() throws Exception {
-        Customer customer = new Customer("Sergii");
-        Rental r1 = new Rental(new Movie("Matrix", Movie.REGULAR), 4);
-        customer.addRental(r1);
-        assertTrue("", customer.statement().contains("Total amount to pay 5.0"));
+        assertTrue("Total amount is incorrect", customer.statement().contains("Total amount to pay 27.5"));
+        assertTrue("Matrix price is incorrect", customer.statement().contains("Matrix\t5.0"));
+        assertTrue("Matrix price is incorrect", customer.statement().contains("Matrix 2\t21.0"));
+        assertTrue("Matrix price is incorrect", customer.statement().contains("Panda Kung Fu\t1.5"));
     }
 
 
 
     @Test
     public void testHtmlStatement() throws Exception {
-        Customer customer = new Customer("Sergii");
-        Rental r1 = new Rental(new Movie("Matrix", Movie.REGULAR), 4);
-        customer.addRental(r1);
-        assertTrue("", customer.htmlStatement().contains("<P>Total amount to pay <EM>5.0</EM><P>"));
+        assertTrue("Total amount is incorrect", customer.htmlStatement()
+                .contains("<P>Total amount to pay <EM>27.5</EM><P>"));
+        assertTrue("Matrix price is incorrect", customer.htmlStatement().contains("Matrix:5.0"));
+        assertTrue("Matrix price is incorrect", customer.htmlStatement().contains("Matrix 2:21.0"));
+        assertTrue("Matrix price is incorrect", customer.htmlStatement().contains("Panda Kung Fu:1.5"));
 
     }
+
+
 }
--- src/com/movierental/Programme.java
+++ src/com/movierental/Programme.java
@@ -5,7 +5,12 @@ public class Programme {
     public static void main(String[] args) {
         Customer customer = new Customer("Sergii");
         Rental r1 = new Rental(new Movie("Matrix", Movie.REGULAR), 4);
+        Rental r2 = new Rental(new Movie("Matrix 2", Movie.NEW_RELEASES), 7);
+        Rental r3 = new Rental(new Movie("Panda Kung Fu", Movie.CHILDRENS), 2);
+
         customer.addRental(r1);
+        customer.addRental(r2);
+        customer.addRental(r3);
         System.out.println(customer.htmlStatement());
     }
 }
